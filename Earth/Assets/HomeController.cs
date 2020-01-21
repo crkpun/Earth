2019648571219fb3df859d7962a1d38c940d7ef1
crkpun <60137084+crkpun@users.noreply.github.com>
2020-01-22 00:04:47 +0800

    介面+地球轉換

---
+++ Earth/Assets/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class HomeController : MonoBehaviour
+{
+    public int[] planetPostions;
+
+    public Transform earths;
+
+    private int _nowPlace = 0;
+
+    public void MoveToLeft()
+    {
+        if (_nowPlace == 0)
+            return;
+        
+        _nowPlace--;
+        earths.DOLocalMoveX(planetPostions[_nowPlace], 0.5f).SetEase(Ease.OutBack);
+    }
+
+    public void MoveToRight()
+    {
+        if (_nowPlace == 4)
+            return;
+        _nowPlace++;
+        earths.DOLocalMoveX(planetPostions[_nowPlace], 0.5f).SetEase(Ease.OutBack);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Earth/Assets/SwitchPlace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class SwitchPlace : MonoBehaviour
+{
+    public Transform[] places;
+
+    private int _nowPlace = 2;
+
+    public void MoveToPlace(int index)
+    {
+        places[_nowPlace].localPosition = new Vector3(0, -1920, 0);
+        places[index].localPosition = new Vector3(0, 0, 0);
+        _nowPlace = index;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
